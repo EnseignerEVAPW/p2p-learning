50a969ca1cbe5c7b83fe2869bfe3b14f162c56df
judithmarg <judithmargaritapacollalli@gmail.com>
2024-04-09 15:46:08 -0400

    add page for login and condition

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup>
 import siteConfig from '../site-config'
 
-const links = siteConfig.profile
+const links = false ? {    ///acá este false debe ser cambiado por una condición que verifique si el usuario está logueado
+    text: 'Perfil',
+    href: '/profile',
+  }: {
+    text: 'Login',
+    href: '/login',
+  }
 </script>
 
 <template>
