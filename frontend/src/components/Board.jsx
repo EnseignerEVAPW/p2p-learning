f6df5eaf68f42b13d2f78a485fb477fff84b749b
judithmarg <judithmargaritapacollalli@gmail.com>
2024-04-17 23:46:21 -0400

    board new

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,91 +1,67 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import { useFetch } from '../services/useFetch';
 import { navigate } from 'astro/virtual-modules/transitions-router.js';
-const auth = 'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_Z7tiAZpXWoNJw7pPapHx1p37aVE';
+import { Tldraw, track, useEditor } from 'tldraw';
+import 'tldraw/tldraw.css';
+import { useYjsStore } from '../services/useYjsStore.ts';
+
+const HOST_URL =
+	import.meta.env.MODE === 'development'
+		? 'ws://localhost:1234'
+		: 'wss://demos.yjs.dev'
 
 function Board() {
-  const [data, setData] = useState(null);
-  const [boardId, setBoardId] = useState(null);
-  const [linkBoard, setLinkBoard] = useState(null);
-  const [itemId, setItemId] = useState("");
-
-  const optionsCreateBoard = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      authorization: 'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_GIFtxnu-o9eQ77RXF9BViNKYtqw'
-    },
-    body: JSON.stringify({ name: 'P2PLearning', description: 'Pizarra de ayuda' })
-  };
-
-  const styles = {
-    myButton: {
-      background: '#d95786',
-      padding: '10px',
-      color: 'white',
-      borderRadius: '10px',
-      borderColor: 'aliceblue',
-      textDecoration: 'none',
-      margin: '10px',
-      boxShadow: `0 4px 8px 0 rgba(0, 0, 0, 0.2),
-        0 6px 20px 0 rgba(0, 0, 0, 0.19)`,
-
-    },
-  };
-
-  useEffect(() => {
-    if (!data) {
-      fetch("https://api.miro.com/v2/boards", optionsCreateBoard)
-        .then(response => response.json())
-        .then(response => {
-          setData(response);
-          setBoardId(response.id);
-          setLinkBoard(response.viewLink);
-        })
-        .catch(err => console.error(err));
+      const store = useYjsStore({
+        roomId: 'example17',
+        hostUrl: HOST_URL,
+      })
+    
+      return (
+        <div className="tldraw__editor">
+          <Tldraw
+            autoFocus
+            store={store}
+            components={{
+              SharePanel: NameEditor,
+            }}
+          />
+        </div>
+      )
     }
-  }, [data]);
-
-  // const { createBoard } = useFetch("https://api.miro.com/v2/boards", optionsCreateBoard); solo se carga, #raro
-
-  const optionsDeleteBoard = {
-    method: 'DELETE',
-    headers: {
-      accept: 'application/json',
-      authorization: 'Bearer eyJtaXJvLm9yaWdpbiI6ImV1MDEifQ_GIFtxnu-o9eQ77RXF9BViNKYtqw'
-    }
-  };
-
-  function deleteBoard() {
-    if (boardId) {
-      const idWithoutLastChar = boardId.slice(0, -1);
-      fetch(`https://api.miro.com/v2/boards/${idWithoutLastChar}%3D`, optionsDeleteBoard)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    }
-    navigate('/feedback');
-  }
-
-  return (
-    <div>
-      <div>
-        {data || linkBoard ?
-          (
-            <>
-              <iframe src={linkBoard || data.viewLink} width="100%" height="700" allowFullScreen></iframe>
-            </>
-          ) :
-          (
-            <p>loading...</p>
-          )}
-      </div>
-      <button style={styles.myButton} onClick={deleteBoard}>TERMINAR</button>
-      <div>
-      </div>
-    </div>
-  );
-}
+    
+    const NameEditor = track(() => {
+      const editor = useEditor()
+    
+      const { color, name } = editor.user.getUserPreferences()
+    
+      return (
+        <div style={{ pointerEvents: 'all', display: 'flex' }}>
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => {
+              editor.user.updateUserPreferences({
+                color: e.currentTarget.value,
+              })
+            }}
+          />
+          <input
+            value={name}
+            onChange={(e) => {
+              editor.user.updateUserPreferences({
+                name: e.currentTarget.value,
+              })
+            }}
+          />
+        </div>
+      )
+    })
+// function Board() {
+//   return (
+//     <div style={{height: 700, width:'100%'}} >
+//       <Tldraw />
+//     </div>
+//   );
+// }
 
 export default Board;
